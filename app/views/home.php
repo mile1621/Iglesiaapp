34e8f56119ad95b1481a4ed8f89716ab08a3c150
mile1621 <133826777+mile1621@users.noreply.github.com>
2025-08-08 11:50:25 -0400

    Update home.php WH

--- app/views/home.php
+++ app/views/home.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenido</title>
     <style>
-        /* TEST GWebhook Two*/
+        /* TEST GWebhook */
         /* Estilos para la barra de navegación */
         .navbar {
             background-color: whitesmoke; /* Color de fondo semi-transparente */
